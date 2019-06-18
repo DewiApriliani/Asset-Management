bc8c0fcf811475f3a50c9e5b9619bc8f7f9720a4
dewiapril1996@gmail.com <dewiapril1996@gmail.com>
2019-06-18 12:51:30 +0700

    commit

---
+++ DataAccess/ViewModel/HandoverVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ViewModel
+{
+    public class HandoverVM
+    {
+        public HandoverVM(string description, string name_User, string name_Admin, DateTimeOffset date_Handover, int user_Id, int admin_Id, int divhead_Id, int loaning_Id, int return_Id)
+        {
+            this.Descriptioan = Descriptioan;
+            this.Name_User = Name_User;
+            this.Name_Admin = Name_Admin;
+            this.Date_Handover = Date_Handover;
+            this.Employee_id = Employee_id;
+            this.Loaning_Id = Loaning_Id;
+            this.Return_Id = Return_Id;
+        }
+
+        public HandoverVM() { }
+
+        public string Descriptioan { get; set; }
+        public string Name_User { get; set; }
+        public string Name_Admin { get; set; }
+        public DateTimeOffset Date_Handover { get; set; }
+        public int Employee_id { get; set; }
+        public int Loaning_Id { get; set; }
+        public int Return_Id { get; set; }
+    }
+}
---
+++ DataAccess/ViewModel/ReturnVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ViewModel
+{
+    public class ReturnVM
+    {
+        public ReturnVM(string name_User, string name_Item, int quantity, string status, string last_Condition, DateTimeOffset date_Return, int user_Id, int item_Id)
+        {
+            this.Name_User = Name_User;
+            this.Name_Item = Name_Item;
+            this.Quantity = Quantity;
+            this.Status = Status;
+            this.Last_Condition = Last_Condition;
+            this.Date_Return = Date_Return;
+            this.Employee_Id = Employee_Id;
+            this.Item_Id = Item_Id;
+        }
+
+        public ReturnVM() { }
+
+        public int Id { get; set; }
+        public string Name_User { get; set; }
+        public string Name_Item { get; set; }
+        public int Quantity { get; set; }
+        public string Status { get; set; }
+        public string Last_Condition { get; set; }
+        public DateTimeOffset Date_Return { get; set; }
+        public int Employee_Id { get; set; }
+        public int Item_Id { get; set; }
+
+    }
+}
