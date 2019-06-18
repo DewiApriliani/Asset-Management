32838e502a0a350b08e6b0efae149a225f115daf
dewiapril1996@gmail.com <dewiapril1996@gmail.com>
2019-06-18 10:00:19 +0700

    Create Models/Handover.cs Create Models/Location.cs Create Models/Parameter.cs Create Models/Return.cs Create Models/TypeItem.cs

---
+++ DataAccess/Models/Handover.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    [Table("TB_T_Handover")]
+    public class Handover
+    {
+        public int Id { get; set; }
+        public string Descriptioam { get; set; }
+        public string Name_User { get; set; }
+        public string Name_Admin { get; set; }
+        public DateTimeOffset Date_Handover { get; set; }
+
+        public int User_id { get; set; }
+        public int Loaning_Id { get; set; }
+        public int Return_Id { get; set; }
+        public int Admin_Id { get; set; }
+        public int Divhead_Id { get; set; }
+    }
+}
---
+++ DataAccess/Models/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    [Table("TB_M_Location")]
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name_location { get; set; }
+        public string floor { get; set; }
+    }
+}
