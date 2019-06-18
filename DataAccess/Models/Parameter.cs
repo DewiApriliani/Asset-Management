a1dcdf96946b1edcbdc08a47e4bd6f713be23dd5
dewiapril1996@gmail.com <dewiapril1996@gmail.com>
2019-06-18 15:05:48 +0700

    commit

--- DataAccess/Models/Parameter.cs
+++ DataAccess/Models/Parameter.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Base;
+using DataAccess.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -13,5 +14,28 @@ namespace DataAccess.Models
     {
         public string Name_Validation { get; set; }
         public string Punishment { get; set; }
+
+        public Parameter() { }
+
+        public Parameter(ParameterVM ParameterVM)
+        {
+            this.Name_Validation = ParameterVM.Name_Validation;
+            this.Punishment = ParameterVM.Punishment;
+            this.CreateDate = DateTimeOffset.Now.ToLocalTime();
+        }
+
+        public void Update(int id, ParameterVM ParameterVM)
+        {
+            this.Id = id;
+            this.Name_Validation = ParameterVM.Name_Validation;
+            this.Punishment = ParameterVM.Punishment;
+            this.UpdateDate = DateTimeOffset.Now.ToLocalTime();
+        }
+
+        public void Delete()
+        {
+            this.IsDelete = true;
+            this.DeleteDate = DateTimeOffset.Now.ToLocalTime();
+        }
     }
 }
