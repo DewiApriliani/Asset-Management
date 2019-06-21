d5215871289e14dbfc8bae8d0848bc85b201bb55
Irfan <irfan.rabbani96@gmail.com>
2019-06-21 09:04:44 +0700

    New Push fixing

--- API/Controllers/ConditionsController.cs
+++ API/Controllers/ConditionsController.cs
@@ -38,10 +38,6 @@ namespace API.Controllers
 
         // GET: api/Conditions/5
         public HttpResponseMessage GetCondition(int id)
-<<<<<<< HEAD
-=======
-
->>>>>>> e46d4412852493ce4924b39873bcc824aa883c07
         {
             var message = Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Bad Request");
             var result = iConditionService.Get(id);
@@ -60,11 +56,11 @@ namespace API.Controllers
             var result = iConditionService.Update(id, conditionVM);
             if (result)
             {
-<<<<<<< HEAD
+
                 message = Request.CreateResponse(HttpStatusCode.OK, conditionVM);
-=======
+
                 message = Request.CreateResponse(HttpStatusCode.OK);
->>>>>>> e46d4412852493ce4924b39873bcc824aa883c07
+
             }
             return message;
         }
@@ -72,19 +68,11 @@ namespace API.Controllers
         // POST: api/Conditions
         public HttpResponseMessage InsertCondition(ConditionVM conditionVM)
         {
-<<<<<<< HEAD
             var message = Request.CreateErrorResponse(HttpStatusCode.NotFound, "Not Found");
             var result = iConditionService.Insert(conditionVM);
             if (result)
             {
                 message = Request.CreateResponse(HttpStatusCode.OK, conditionVM);
-=======
-            var message = Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Bad Request");
-            var result = iConditionService.Insert(conditionVM);
-            if (result)
-            {
-                message = Request.CreateResponse(HttpStatusCode.Created);
->>>>>>> e46d4412852493ce4924b39873bcc824aa883c07
             }
             return message;
         }
